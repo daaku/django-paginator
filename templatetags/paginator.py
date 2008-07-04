f8847ccf4e0e6edeb3e8708b99af1ba268519167
Naitik Shah <naitik.shah@yahoo.com>
2008-07-03 21:20:22 -0700

    remove indirection variables, use page object directly - template cleanup

--- templatetags/paginator.py
+++ templatetags/paginator.py
@@ -10,39 +10,32 @@ ADJACENT_PAGES = 4
 
 @register.inclusion_tag('paginator/paginator.html', takes_context=True)
 def paginator(context):
-    paginator = context['paginator']
     page = context['jewelry']
-    current_page_number = page.number
+    num_pages = page.paginator.num_pages
 
     if (page.has_other_pages()):
         in_leading_range = in_trailing_range = False
         pages_outside_leading_range = pages_outside_trailing_range = range(0)
 
-        if (paginator.num_pages <= LEADING_PAGE_RANGE_DISPLAYED):
+        if (num_pages <= LEADING_PAGE_RANGE_DISPLAYED):
             in_leading_range = in_trailing_range = True
-            page_numbers = [n for n in range(1, paginator.num_pages + 1) if n > 0 and n <= paginator.num_pages]
-        elif (current_page_number <= LEADING_PAGE_RANGE):
+            page_numbers = [n for n in range(1, num_pages + 1) if n > 0 and n <= num_pages]
+        elif (page.number <= LEADING_PAGE_RANGE):
             in_leading_range = True
-            page_numbers = [n for n in range(1, LEADING_PAGE_RANGE_DISPLAYED + 1) if n > 0 and n <= paginator.num_pages]
-            pages_outside_leading_range = [n + paginator.num_pages for n in range(0, -NUM_PAGES_OUTSIDE_RANGE, -1)]
-        elif (current_page_number > paginator.num_pages - TRAILING_PAGE_RANGE):
+            page_numbers = [n for n in range(1, LEADING_PAGE_RANGE_DISPLAYED + 1) if n > 0 and n <= num_pages]
+            pages_outside_leading_range = [n + num_pages for n in range(0, -NUM_PAGES_OUTSIDE_RANGE, -1)]
+        elif (page.number > num_pages - TRAILING_PAGE_RANGE):
             in_trailing_range = True
-            page_numbers = [n for n in range(paginator.num_pages - TRAILING_PAGE_RANGE_DISPLAYED + 1, paginator.num_pages + 1) if n > 0 and n <= paginator.num_pages]
+            page_numbers = [n for n in range(num_pages - TRAILING_PAGE_RANGE_DISPLAYED + 1, num_pages + 1) if n > 0 and n <= num_pages]
             pages_outside_trailing_range = [n + 1 for n in range(0, NUM_PAGES_OUTSIDE_RANGE)]
         else:
-            page_numbers = [n for n in range(current_page_number - ADJACENT_PAGES, current_page_number + ADJACENT_PAGES + 1) if n > 0 and n <= paginator.num_pages]
-            pages_outside_leading_range = [n + paginator.num_pages for n in range(0, -NUM_PAGES_OUTSIDE_RANGE, -1)]
+            page_numbers = [n for n in range(page.number - ADJACENT_PAGES, page.number + ADJACENT_PAGES + 1) if n > 0 and n <= num_pages]
+            pages_outside_leading_range = [n + num_pages for n in range(0, -NUM_PAGES_OUTSIDE_RANGE, -1)]
             pages_outside_trailing_range = [n + 1 for n in range(0, NUM_PAGES_OUTSIDE_RANGE)]
         return {
             'base_url': 'blah?page=', #FIXME
-            'is_paginated': page.has_other_pages(),
-            'previous': page.previous_page_number(),
-            'has_previous': page.has_previous(),
-            'next': page.next_page_number(),
-            'has_next': page.has_next(),
+            'page': page,
             'results_per_page': 6, #FIXME
-            'current_page_number': current_page_number,
-            'pages': paginator.num_pages,
             'page_numbers': page_numbers,
             'in_leading_range' : in_leading_range,
             'in_trailing_range' : in_trailing_range,
